68bef3b5498a889b05c42faa5ff4dc33d572b658
Charlesw06 <cwalford3884@bths.edu>
2022-11-03 18:29:45 -0400

    Added countString, removeString, and replaceCharacter methods to StringLoops class

--- src/StringLoops.java
+++ src/StringLoops.java
@@ -47,11 +47,34 @@ public class StringLoops {
     public int countString(String searchString, String origString)
     {
         int count = 0;
-        while (origString.indexOf(searchString) != -1) {
-            origString = origString.substring(1, origString.indexOf(searchString)) + origString.substring(origString.indexOf(searchString) + searchString.length());
-            count++;
+        String origLowCase = origString.toLowerCase();
+        String searchLowCase = searchString.toLowerCase();
+        String index;
+
+        for (int i = 0; i < origLowCase.length();i++) {
+            if (i + searchLowCase.length() <= origLowCase.length()) {
+                index = origLowCase.substring(i, i + searchLowCase.length());
+                if (index.equals(searchLowCase)) {
+                    count++;
+                }
+            }
         }
         return count;
     }
 
+    public String removeString(String searchString, String origString)
+    {
+        while (origString.indexOf(searchString) != -1) {
+            origString = origString.substring(0, origString.indexOf(searchString)) + origString.substring(origString.indexOf(searchString) + searchString.length());
+        }
+        return origString;
+    }
+
+    public String replaceCharacter(String searchChar, String origStr, String replaceChar)
+    {
+        while (origStr.indexOf(searchChar) != -1) {
+            origStr = origStr.substring(0, origStr.indexOf(searchChar)) + replaceChar + origStr.substring(origStr.indexOf(searchChar) + 1);
+        }
+        return origStr;
+    }
 }
